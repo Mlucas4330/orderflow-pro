c3a69b4615ae906d6ee47cfe4a252e30f6a45b23
mlucas4330 <mlucas4330@gmail.com>
2025-07-11 01:04:41 -0300

    fix: some imports were not working

--- internal/model/order_item.go
+++ internal/model/order_item.go
@@ -6,9 +6,9 @@ import (
 )
 
 type OrderItem struct {
-    ID          uuid.UUID       `db:"id"`
-    OrderID     uuid.UUID       `db:"order_id"`
-    ProductID   uuid.UUID       `db:"product_id"`
-    Quantity    int             `db:"quantity"`
-    PriceAtTime decimal.Decimal `db:"price_at_time"`
+	ID          uuid.UUID       `db:"id"`
+	OrderID     uuid.UUID       `db:"order_id"`
+	ProductID   uuid.UUID       `db:"product_id"`
+	Quantity    int             `db:"quantity"`
+	PriceAtTime decimal.Decimal `db:"price_at_time"`
 }
